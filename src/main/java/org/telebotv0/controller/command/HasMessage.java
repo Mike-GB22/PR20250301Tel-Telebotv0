e6cb34785cbe29262b47f622f9c2821fdfd4af4c
Mipusatina <mipspam@mail.ru>
2025-03-02 10:39:44 +0100

    Commands. HasMessage and HasPhoto v1

--- src/main/java/org/telebotv0/controller/command/HasMessage.java
+++ src/main/java/org/telebotv0/controller/command/HasMessage.java
@@ -1,10 +1,13 @@
 package org.telebotv0.controller.command;
 
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
 public class HasMessage implements Command {
+    private static final String CAPTION = "*Update has Message.*";
+
     @Override
     public boolean isApplicable(Update update) {
         return update.hasMessage();
@@ -16,6 +19,17 @@ public class HasMessage implements Command {
             return "Вызов не к месту.";
         }
 
-        return "Update has Message.";
+        Message msg = update.getMessage();
+        StringBuilder info = new StringBuilder(CAPTION)
+                .append("\n - msg.getChatId(): ").append(msg.getChatId())
+                .append("\n - msg.getMessageId(): ").append(msg.getMessageId())
+                .append("\n - msg.getMessageThreadId(): ").append(msg.getMessageThreadId())
+                .append("\n - msg.getMediaGroupId(): ").append(msg.getMediaGroupId())
+                .append("\n - msg.getForwardFromMessageId(): ").append(msg.getForwardFromMessageId())
+                .append("\n - msg.getMigrateFromChatId(): ").append(msg.getMigrateFromChatId())
+                .append("\n - msg.getMigrateToChatId(): ").append(msg.getMigrateToChatId())
+                .append("\n");
+
+        return info.toString();
     }
 }
--- src/main/java/org/telebotv0/controller/command/HasMessageHasPhoto.java
+++ src/main/java/org/telebotv0/controller/command/HasMessageHasPhoto.java
@@ -1,10 +1,18 @@
 package org.telebotv0.controller.command;
 
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class HasMessageHasPhoto implements Command {
+    private static final String CAPTION = "*Update has Message. Message has Photo.*";
+
     @Override
     public boolean isApplicable(Update update) {
         return update.hasMessage() && update.getMessage().hasPhoto();
@@ -16,6 +24,36 @@ public class HasMessageHasPhoto implements Command {
             return "Вызов не к месту.";
         }
 
-        return "Update has Message. Message has Photo";
+        Message msg = update.getMessage();
+        List<PhotoSize> photoSizes = msg.getPhoto();
+        StringBuilder info = new StringBuilder(CAPTION)
+                .append("\n - photoSize.size(): ").append(photoSizes.size())
+                ;
+        Set<String> fileIds = new HashSet<>();
+        boolean flagAllFileIdIsTheSame = true;
+        String previousFileId = null;
+        for (int i = 0; i < photoSizes.size(); i++) {
+            PhotoSize photo = photoSizes.get(i);
+
+            String fileId = photo.getFileId();
+            fileIds.add(fileId);
+            if (previousFileId != null && !previousFileId.equals(fileId)) {
+                flagAllFileIdIsTheSame = false;
+            } else {
+                previousFileId = fileId;
+            }
+
+            info.append(String.format("\n # photoSize.get(%d): ", i))
+                    .append("\nfileId: `").append(fileId).append("`")
+                    .append("\nfileUniqueId: ").append(photo.getFileUniqueId())
+                    .append("\nwidth x height: ").append(photo.getWidth()).append(" x ").append(photo.getHeight())
+                    .append("\nfileSize: ").append(photo.getFileSize())
+                    .append("\nfilePath: ").append(photo.getFilePath())
+                    .append("\n");;
+        }
+        info.append(" = All photoSize has the same fileId: ").append(flagAllFileIdIsTheSame)
+                .append(fileIds);
+
+        return info.toString();
     }
 }
